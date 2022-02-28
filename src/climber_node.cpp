c532f1eda8c258da24fe44a5b6c642d0ec4f6109
frc195programming <frc195programming@gmail.com>
2022-02-28 02:12:59 +0000

    initial climb state machine

--- src/climber_node.cpp
+++ src/climber_node.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <string>
 #include <mutex>
+#include <map>
 #include "hmi_agent_node/HMI_Signals.h"
 #include "ck_utilities/Motor.hpp"
 #include "ck_utilities/Piston.hpp"
@@ -24,6 +25,7 @@
 
 ros::NodeHandle* node;
 
+static constexpr double CLIMBER_BALANCE_GAIN = 0.0795;
 
 static Motor* left_climber_master;
 static Motor* left_climber_follower;
@@ -43,7 +45,11 @@ enum class ClimberStates
 	GRAB_INITIAL_BAR,
 	PULL_UP,
 	STATIC_LATCH,
-	GRAB_NEXT_BAR,
+	GRAB_NEXT_BAR_INITIAL_UNWINCH,
+	GRAB_NEXT_BAR_EXTEND_PISTONS,
+	GRAB_NEXT_BAR_UNWINCH_COMPLETELY,
+	GRAB_NEXT_BAR_RETRACT_PISTONS,
+	GRAB_NEXT_BAR_PULL_UP,
 	STATIC_UNLATCH,
 	END,
 	STOPPED
@@ -58,12 +64,35 @@ static bool deploy_hooks = false;
 static bool begin_climb = false;
 static bool retract_hooks = false;
 
+static std::map<uint16_t, rio_control_node::Motor_Info> motor_status_map;
+static double left_climber_position = 0.0;
+static double right_climber_position = 0.0;
+
 void hmi_signal_callback(const hmi_agent_node::HMI_Signals& msg)
 {
     stop_climber = msg.stop_climber || stop_climber;
 	deploy_hooks = msg.deploy_hooks;
 	begin_climb = msg.begin_climb;
 	retract_hooks = msg.retract_hooks;
+}
+
+void motor_status_callback(const rio_control_node::Motor_Status& msg)
+{
+	for ( const rio_control_node::Motor_Info& motorInfo : msg.motors )
+	{
+		motor_status_map[motorInfo.id] = motorInfo;
+	}
+
+	if ( motor_status_map.count(LEFT_CLIMBER_MASTER_CAN_ID) && motor_status_map.count(RIGHT_CLIMBER_MASTER_CAN_ID) )
+	{
+		left_climber_position = motor_status_map [LEFT_CLIMBER_MASTER_CAN_ID].sensor_position;
+		right_climber_position = motor_status_map [RIGHT_CLIMBER_MASTER_CAN_ID].sensor_position;
+	}
+	else
+	{
+		left_climber_position = 0.0;
+		right_climber_position = 0.0;
+	}
 }
 
 void imu_sensor_callback(const nav_msgs::Odometry& msg)
@@ -78,9 +107,11 @@ void imu_sensor_callback(const nav_msgs::Odometry& msg)
 	(void) yaw;
 }
 
-void auto_balance_climb()
+void auto_balance_climb(double target_position)
 {
-
+	double arbFF = CLIMBER_BALANCE_GAIN * imu_roll_rad;
+	left_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, target_position, arbFF);
+	right_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, target_position, -arbFF);
 }
 
 void step_state_machine()
@@ -94,35 +125,88 @@ void step_state_machine()
 			left_climber_master->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
 			right_climber_master->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
 			climber_solenoid->set(Piston::PistonState::OFF);
+		
+			if(deploy_hooks)
+			{
+				climber_state = ClimberStates::DEPLOY_INITIAL_HOOKS;
+			}
+
 			break;
 		}
 
 		case ClimberStates::DEPLOY_INITIAL_HOOKS: //Winch up hooks
 		{
-			left_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, -1.5, 0);
-			right_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, -1.5, 0);
+			left_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 15, 0);
+			right_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 15, 0);
+
+			if(left_climber_position > 14.5 && right_climber_position > 14.5)
+			{
+				climber_state = ClimberStates::GRAB_INITIAL_BAR;
+			}
 			break;
 		}
 
 		case ClimberStates::GRAB_INITIAL_BAR: //no op
 		{
+			climber_state = ClimberStates::PULL_UP;
 			break;
 		}
 
 		case ClimberStates::PULL_UP: //Winch down hooks
 		{
+			auto_balance_climb(4.0);
 
+			if(left_climber_position < 4.5 && right_climber_position < 4.5)
+			{
+				climber_state = ClimberStates::STATIC_LATCH;
+			}
 			break;
 		}
 
 		case ClimberStates::STATIC_LATCH://Continue to pull up
 		{
+			climber_state = ClimberStates::GRAB_NEXT_BAR_INITIAL_UNWINCH;
 			break;
 		}
 
-		case ClimberStates::GRAB_NEXT_BAR:
-		//unwinch a little bit, activate pistons, unwinch a lot, actuate pistons, pull up
+		case ClimberStates::GRAB_NEXT_BAR_INITIAL_UNWINCH://unwinch a little bit
 		{
+			left_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 6.0, 0);
+			right_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 6.0, 0);
+
+			if(right_climber_position > 5.5 && left_climber_position > 5.5)
+			{
+				climber_state = ClimberStates::GRAB_NEXT_BAR_EXTEND_PISTONS;
+			}
+			break;
+		}
+
+		case ClimberStates::GRAB_NEXT_BAR_EXTEND_PISTONS://activate pistons, 
+		{
+			climber_solenoid->set(Piston::PistonState::ON);
+
+			if()
+			break;
+		}
+
+		case ClimberStates::GRAB_NEXT_BAR_UNWINCH_COMPLETELY://uwinch a lot
+		{
+			
+			left_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 15, 0);
+			right_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 15, 0);
+			break;
+		}
+
+		case ClimberStates::GRAB_NEXT_BAR_RETRACT_PISTONS://actuate pistons
+		{
+			climber_solenoid->set(Piston::PistonState::OFF);
+			break;
+		}
+
+
+		case ClimberStates::GRAB_NEXT_BAR_PULL_UP://pull up
+		{
+			auto_balance_climb(4.0);
 			break;
 		}
 
@@ -133,16 +217,20 @@ void step_state_machine()
 
 		case ClimberStates::END://End
 		{
+			//deploy fireworks
 			break;
 		}
 
 		case ClimberStates::RETRACT_HOOKS://Winch down hooks, go to idle
 		{
+			left_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 4.0, 0);
+			right_climber_master->set(Motor::Control_Mode::MOTION_MAGIC, 4.0, 0);
 			break;
 		}
 
 		case ClimberStates::STOPPED://Turn off all motors and stay there
 		{
+			
 			break;
 		}
 	}
@@ -170,6 +258,7 @@ int main(int argc, char **argv)
 
 	ros::Subscriber hmi_subscribe = node->subscribe("/HMISignals", 1, hmi_signal_callback);
 	ros::Subscriber imu_subscribe = node->subscribe("/RobotIMU", 1, imu_sensor_callback);
+	ros::Subscriber motor_status_subscribe = node->subscribe("/MotorStatus", 1, motor_status_callback);
 
 	left_climber_master = new Motor(LEFT_CLIMBER_MASTER_CAN_ID, Motor::Motor_Type::TALON_FX);
 	right_climber_master = new Motor(RIGHT_CLIMBER_MASTER_CAN_ID, Motor::Motor_Type::TALON_FX);
